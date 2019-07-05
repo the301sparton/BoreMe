bedac0019a6144d4d8cfb56b7dd5d741723a255a
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-07-06 04:49:16 +0530

    Add Google Login for user authentication

--- app/src/main/java/com/example/boreme/MainActivity.java
+++ app/src/main/java/com/example/boreme/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.boreme;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                200);
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 200) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(getApplicationContext(),"Sign In successful :)", Toast.LENGTH_SHORT).show();
+                // ...
+            } else {
+                Toast.makeText(getApplicationContext(), "Failed To Sign In :(", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
