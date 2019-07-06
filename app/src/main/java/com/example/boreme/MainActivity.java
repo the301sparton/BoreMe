aafd95e3375976915ce207916bfddb68d2224f3a
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-07-06 05:51:54 +0530

    Set username, photo, email from google account

--- app/src/main/java/com/example/boreme/MainActivity.java
+++ app/src/main/java/com/example/boreme/MainActivity.java
@@ -3,24 +3,42 @@ package com.example.boreme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
 
+public class MainActivity extends AppCompatActivity {
+    CircleImageView meImg;
+    TextView displayName, emailId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meImg = findViewById(R.id.profile_image);
+        displayName = findViewById(R.id.displayName);
+        emailId = findViewById(R.id.emailId);
 
         List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
 
@@ -44,6 +62,14 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+                if (user != null) {
+                    Log.i("url", String.valueOf(user.getPhotoUrl()));
+                    displayName.setText(user.getDisplayName());
+                    emailId.setText(user.getEmail());
+                    Picasso.get().load(user.getPhotoUrl()).into(meImg);
+                }
                 Toast.makeText(getApplicationContext(),"Sign In successful :)", Toast.LENGTH_SHORT).show();
                 // ...
             } else {
@@ -51,4 +77,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 }
